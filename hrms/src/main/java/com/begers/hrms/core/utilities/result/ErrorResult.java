4a09715c0befe852f84b61f3a7cac1203008ac5d
bekir geriş <bekir.geris@gmail.com>
2021-05-25 21:53:10 +0300

    Result sınıfları tamamalandı

---
+++ hrms/src/main/java/com/begers/hrms/core/utilities/result/ErrorResult.java
@@ -0,0 +1,13 @@
+package com.begers.hrms.core.utilities.result;
+
+public class ErrorResult extends Result{
+
+	public ErrorResult() {
+		super(false);
+	}
+
+	public ErrorResult(boolean success, String message) {
+		super(false, message);
+	}
+	
+}
