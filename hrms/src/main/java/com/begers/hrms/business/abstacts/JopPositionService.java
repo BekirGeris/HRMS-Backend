76cf20c7cb59e18a3d0a412a934c9fa7a1f4e5e5
bekir geriş <bekir.geris@gmail.com>
2021-05-26 17:34:42 +0300

    Service interfaces have been added to the usiness/astracts of the project.

--- hrms/src/main/java/com/begers/hrms/business/abstacts/JopPositionService.java
+++ hrms/src/main/java/com/begers/hrms/business/abstacts/JopPositionService.java
@@ -2,9 +2,11 @@ package com.begers.hrms.business.abstacts;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.begers.hrms.entites.concoretes.JopPositions;
 
+@Service
 public interface JopPositionService {
-
 	List<JopPositions> gatAll();
 }
---
+++ hrms/src/main/java/com/begers/hrms/business/abstacts/UserService.java
@@ -0,0 +1,8 @@
+package com.begers.hrms.business.abstacts;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+}
