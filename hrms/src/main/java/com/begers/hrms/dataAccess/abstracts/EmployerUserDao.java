f0159481f510a8940667609a3b2b20c0bd9f8ddb
bekir geriş <bekir.geris@gmail.com>
2021-05-26 16:17:51 +0300

    Repository interfaces have been added into the project's dataAccess/abstracts.

--- hrms/src/main/java/com/begers/hrms/dataAccess/abstracts/EmployerUserDao.java
+++ hrms/src/main/java/com/begers/hrms/dataAccess/abstracts/EmployerUserDao.java
@@ -3,9 +3,9 @@ package com.begers.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.begers.hrms.entites.concoretes.JopPositions;
+import com.begers.hrms.entites.concoretes.EmployerUser;
 
 @Repository
-public interface JopPositionsDao extends JpaRepository<JopPositions, Integer>{
+public interface EmployerUserDao extends JpaRepository<EmployerUser, Integer>{
 
 }
--- hrms/src/main/java/com/begers/hrms/dataAccess/abstracts/JopSeekersDao.java
+++ hrms/src/main/java/com/begers/hrms/dataAccess/abstracts/JopSeekersDao.java
@@ -3,9 +3,9 @@ package com.begers.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.begers.hrms.entites.concoretes.JopPositions;
+import com.begers.hrms.entites.concoretes.JopSeekers;
 
 @Repository
-public interface JopPositionsDao extends JpaRepository<JopPositions, Integer>{
+public interface JopSeekersDao extends JpaRepository<JopSeekers, Integer>{
 
 }
