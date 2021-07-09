9badd8aa925eb49cb5df22761fe6241aa6bb5228
bekir geriş <bekir.geris@gmail.com>
2021-07-09 23:42:37 +0300

    code refactor

--- hrms/src/main/java/com/begers/hrms/api/controllers/CvController.java
+++ hrms/src/main/java/com/begers/hrms/api/controllers/CvController.java
@@ -67,13 +67,13 @@ public class CvController {
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDataResult<Object> handleValidationException
-            (MethodArgumentNotValidException exceptions){
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
         Map<String,String> validationErrors = new HashMap<String, String>();
+        
         for(FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
             validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
-
+        
         ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors,"Validation errors");
         return errors;
     }
